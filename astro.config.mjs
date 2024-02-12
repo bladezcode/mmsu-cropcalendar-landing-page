d112b56f1daf57c2584183f7e8236da4a209d5f0
Your Name <meugene992@gmail.com>
2024-02-12 12:55:47 +0800

    version1

---
+++ astro.config.mjs
@@ -0,0 +1,21 @@
+import { defineConfig } from 'astro/config';
+
+import tunnel from "astro-tunnel";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tunnel({
+    // The URL to expose. Defaults to http://localhost:4321.
+    url: 'http://localhost:4321',
+    // The local server port to expose. Defaults to 4321. Only used if `url` is not set.
+    port: 4321,
+    // The local server hostname to expose. Defaults to localhost. Only used if `url` is not set.
+    host: 'localhost',
+    // The local server protocol to use. Defaults to http. Only used if `url` is not set.
+    protocol: 'http',
+    // Whether to verify the local server TLS certificate. Defaults to false.
+    verifyTLS: false,
+    // Whether to accept the Cloudflare terms of service. Defaults to false.
+    acceptCloudflareNotice: true,
+  })]
+});
